cc5efcf9f88b6798539c7db4309874a376ae2d94
lim42 <apple@dog.com>
2017-04-25 21:22:40 +0800

    [+]嵌入dll资源到exe中:art:

--- Program.cs
+++ Program.cs
@@ -1,35 +1,39 @@
-﻿using iTextSharp.text;
-using iTextSharp.text.pdf;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
-namespace PDFSharp
+namespace UnlockPDF
 {
-    class Program
+    internal class Program
     {
-        [STAThreadAttribute]
-        static void Main(string[] args)
+        [STAThread]
+        private static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("本程序支持批量解除PDF编辑限制，处理后自动覆盖原文件!\n『C0de by 工艺系统室热工组hangch Ver1.0』\n");
-
-            var pdfFiles = new List<string>();
+            Console.WriteLine("本程序支持批量解除PDF编辑限制，处理完自动覆盖原文件。");
+            Console.WriteLine("Tips:由于嵌入了dll资源，360可能会报毒，请忽略之!");
+            Console.WriteLine("『C0de by 工艺系统室-热工组-hangch Ver1.1』\n");
 
             var ofd = new OpenFileDialog()
             {
                 Multiselect = true,
                 RestoreDirectory = true,
-                Filter = "PDF文件|*.pdf|所有文件|*.*",
+                Filter = "PDF文件|*.pdf",
                 FilterIndex = 1
             };
+            var pdfFiles = new List<string>();
             if (DialogResult.OK == ofd.ShowDialog())
             {
                 pdfFiles = new List<string>(ofd.FileNames);
             }
 
-            string exceptionMessage = string.Empty;
+            AppDomain.CurrentDomain.AssemblyResolve += LoadManagedDllResource;
+
+            var exceptionMessage = string.Empty;
             foreach (var pdfFile in pdfFiles)
             {
                 if (DecryptPDF(pdfFile, ref exceptionMessage))
@@ -40,7 +44,7 @@ namespace PDFSharp
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("[-]Failed  --> " + Path.GetFileName(pdfFile)+ " >>>> Exception: " + exceptionMessage);
+                    Console.WriteLine("[-]Failed  --> " + Path.GetFileName(pdfFile) + " >>>> Exception: " + exceptionMessage);
                 }
             }
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -57,7 +61,7 @@ namespace PDFSharp
 
                 var reader = new PdfReader(pdfFile);
                 PdfReader.unethicalreading = true;
-                
+
                 var document = new Document(reader.GetPageSize(1), 50, 50, 50, 50);
                 // 创建该文档
                 var writer = PdfWriter.GetInstance(document, new FileStream(tmpFile, FileMode.Create));
@@ -83,7 +87,20 @@ namespace PDFSharp
                 return false;
             }
 
-            return true;          
+            return true;
+        }
+
+        private static Assembly LoadManagedDllResource(object sender, ResolveEventArgs args)
+        {
+            var dllName = MethodBase.GetCurrentMethod().DeclaringType.Namespace +
+                "." +
+                new AssemblyName(args.Name).Name + ".dll";
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(dllName))
+            {
+                var buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+                return Assembly.Load(buffer);
+            }
         }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的一般信息由以下
@@ -22,7 +21,7 @@ using System.Runtime.InteropServices;
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("87348272-c9f5-451b-aec7-eb374f37d9ed")]
 
-// 程序集的版本信息由下列四个值组成: 
+// 程序集的版本信息由下列四个值组成:
 //
 //      主版本
 //      次版本
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0")]
+[assembly: AssemblyVersion("1.1")]
+[assembly: AssemblyFileVersion("1.1")]
